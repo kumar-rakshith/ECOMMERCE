a11d656a2af46785ba5b615189a1530ab46efb40
kumar-rakshith <rakshithkumar.thokur@gmail.com>
2024-07-25 18:54:28 +0530

    ADDING FILES

---
+++ FRONTEND/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { useContext, useState } from 'react';
+import './Navbar.css'
+import logo from "../Assets/logo.png"
+import cart_icon from "../Assets/cart_icon.png";
+import { Link } from 'react-router-dom';
+import { ShopContext } from '../../Context/ShopContext';
+
+const Navbar = () => {
+
+    const [menu, setMenu] = useState("Shop")
+    const {getTotalCartItems} = useContext(ShopContext)
+
+    return (
+        <div className='navbar'>
+            <div className='nav-logo'>
+                <Link to={"/"}><img src={logo} alt="" /></Link>
+                <Link to={"/"}><p>SHOPPER</p></Link>
+            </div>
+            <ul className='nav-menu'>
+                <li onClick={() => {setMenu("Shop")}}><Link style={{textDecoration:"none"}} to='/'>Shop</Link>{menu==="Shop"?<hr/>:<></>}</li>
+                <li onClick={() => {setMenu("Men")}}><Link style={{textDecoration:"none"}} to='/mens'>Men</Link>{menu==="Men"?<hr/>:<></>}</li>
+                <li onClick={() => {setMenu("Women")}}><Link style={{textDecoration:"none"}} to='/womens'>Women</Link>{menu==="Women"?<hr/>:<></>}</li>
+                <li onClick={() => {setMenu("Kids")}}><Link style={{textDecoration:"none"}} to='/kids'>Kids</Link>{menu==="Kids"?<hr/>:<></>}</li>
+            </ul>
+            <div className='nav-login-cart'>
+                <Link to="/login"><button>Login</button></Link>
+                <Link to='/cart'><img src={cart_icon} alt="" /></Link>
+                <div className="nav-cart-count">{getTotalCartItems()}</div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# ECOMMERCE-
+# ECOMMERCE- Website with React JS
+Overview
+This repository contains the source code for an eCommerce website built using React JS. The website offers various features, including displaying trending products, product categories for men, women, and kids, a single product page with detailed information, a shopping cart, and a user login/registration system.
+
+Features
+Home Page: The home page displays trending products, an offer banner, and a newsletter subscription form.
+
+Product Pages: Separate pages for men's, women's, and kids' categories showcasing a list of products.
+
+Single Product Page: Detailed product page with product image gallery, pricing, description, and related product recommendations.
+
+Shopping Cart Page: Displays the products added to the cart and allows users to review and manage their cart.
+
+Login/Register Page: Enables users to create an account or log in to the eCommerce website.
+
